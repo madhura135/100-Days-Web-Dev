7a8fcd503f02b7c958859e4a7bbb2a10ad7edd59
Madhura Belsare <madhura135200@gmail.com>
2026-04-23 23:54:05 +0530

    .

--- DAY-21_JS_Practice/exe-2.js
+++ DAY-21_JS_Practice/exe-2.js
@@ -1,8 +1,8 @@
 // 35. Check Character Between 2nd and 4th Positions in String
 // Write a program to check whether a specified character exists between the 2nd and 4th positions in a given string.
 
-let str = prompt("Enter the string:");
-let speciChar = prompt("Enter the string:");
+let str;
+let speciChar;
 function checkSpeciCharExists(str, speciChar) {
   if (str.length < 2) {
     console.log("String length is too short" + str);
