197ccb6fa71ac629ee098ac754b1bca749949954
Madhura Belsare <madhura135200@gmail.com>
2026-07-27 22:04:43 +0530

    updated

--- DAY-37_JS/script.js
+++ DAY-37_JS/script.js
@@ -1,34 +1,33 @@
-//grab the textarea
+// Grab the textarea
 const inputArea = document.getElementById("textArea");
-//to click on check
+
+// Grab the check button
 const checkBtn = document.getElementById("Check");
 
-//to grab where we need to paste output
-
+// Grab where we need to show output
 const output = document.getElementById("result");
 
-//grab value from textarea
-const stringValue = inputArea.value;
-
 function pallindromeLogic(stringValue) {
-  function splitString() {
-    //1.split
-    let r1 = stringValue.split();
-    return r1;
-  }
-  //2.reverse
-  function reverseString() {
-    let r2 = r1.reverse();
-    return r2;
-  }
-  //3.join
-  function joinString() {
-    let r3 = r2.join();
-    return r3;
-  }
+  // 1. Split
+  let r1 = stringValue.split("");
+
+  // 2. Reverse
+  let r2 = r1.reverse();
+
+  // 3. Join
+  let r3 = r2.join("");
+
+  // 4. Compare
   if (r3 === stringValue) {
-    return (output.textContent = `Result: This is a Pallindrome`);
+    output.textContent = `Result: This is a Palindrome`;
   } else {
-    return (output.textContent = `Result: This is not a Pallindrome`);
+    output.textContent = `Result: This is not a Palindrome`;
   }
 }
+
+checkBtn.addEventListener("click", function () {
+  // Grab value AFTER the user clicks
+  const stringValue = inputArea.value;
+
+  pallindromeLogic(stringValue);
+});
