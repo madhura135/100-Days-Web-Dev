c4db2f38cf7aecced04dd815bd5f60f1bca9eb37
Madhura Belsare <madhura135200@gmail.com>
2026-09-06 17:05:09 +0530

    added

---
+++ DAY-43_JS_20Ques/exe-1.js
@@ -0,0 +1,5 @@
+//Given const point = [10, 20, 30];, extract x, y, z in one line.
+//Array Destructuring
+const point = [10, 20, 30];
+let[x,y,z]=point;
+console.log(x,y,z);
---
+++ DAY-43_JS_20Ques/exe-2.js
@@ -0,0 +1,5 @@
+// Given const user = { name: "Priya", age: 28, city: "Pune" };, pull out just name and city.
+//Object Destructuring
+const user = { name: "Priya", age: 28, city: "Pune" };
+let{name,city}=user;
+console.log(name,city);
